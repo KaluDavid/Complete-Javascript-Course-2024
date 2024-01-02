62d1e16fd1280ff9887811ac33a9957b6b5334de
Kalu David <kaludavidinyang@gmail.com>
2024-01-02 20:21:29 +0100

    first assignment

---
+++ javascrip fundamentals, part 1/assigment.js
@@ -0,0 +1,16 @@
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+// Answer
+// Q1
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = '200 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
