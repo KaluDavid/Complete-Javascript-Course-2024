2186582cf5cbed2ade473dc10a80331c725fc5d4
Kalu David <kaludavidinyang@gmail.com>
2024-01-03 22:33:50 +0100

    assignment

--- javascrip fundamentals, part 1/basic operators/precedence.js
+++ javascrip fundamentals, part 1/basic operators/precedence.js
@@ -17,3 +17,26 @@ m tall.
 */
 
 // solution
+// 1. Store Mark's and John's mass and height in variables
+//Mark's mass and height
+let markMass = 78;
+let markHeight = 1.69;
+
+// john's mass and height
+let johnMass = 92;
+let johnHeight = 1.92;
+
+// 2. Calculate both their BMIs using the formula (you can even implement both versions)
+// formula:  BMI = mass / height ** 2 = mass / (height * height)
+
+const markBmi = markMass / markHeight ** 2;
+console.log(markBmi);
+
+const johnBmi = johnMass / johnHeight ** 2;
+console.log(johnBmi);
+
+console.log(markBmi > johnBmi)
+
+// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+const markHigherBMI = markBmi > johnBmi;
+console.log(markHigherBMI);
