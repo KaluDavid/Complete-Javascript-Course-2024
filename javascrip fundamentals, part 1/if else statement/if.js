67ad1c8a9199620d0c3c65dc19297eef516701a6
Kalu David <kaludavidinyang@gmail.com>
2024-01-04 12:58:51 +0100

    if else commit

---
+++ javascrip fundamentals, part 1/if else statement/if.js
@@ -0,0 +1,31 @@
+// if statement
+/*
+if () {
+
+}
+()-is the condition that if true will execute the code inside {}
+the condition is about a boolean 
+NOTE if the condition is false, it will automatically move to the next line, to execute the else statement
+*/
+
+// example
+// lets say david is ready to marry or not
+const ageForMarriage = 30;
+
+if (ageForMarriage >= 28) {
+    const marriage = `Yes David can get married`
+    console.log(marriage); // this prints that if david is at the age of 28 - 30, he can get married
+}
+
+// now lets create an else statement
+// lets say david is not of age to marry
+const marriageAge = 27;
+let marriageCondition;
+let personAge = 90;
+if (marriageAge <= personAge) {
+    marriageCondition = `Yes, David is ready for marriage`
+} else {
+    const yearsLeft = marriageAge - personAge;
+    marriageCondition = `Wait ${yearsLeft} more years before you marry`;
+}
+console.log(marriageCondition)
