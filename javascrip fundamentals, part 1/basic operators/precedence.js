0a6becc8f47fff1b43c2b2e29f5c2ccccfd3ba59
Kalu David <kaludavidinyang@gmail.com>
2024-01-04 11:16:39 +0100

    complete assignment commmit

--- javascrip fundamentals, part 1/basic operators/precedence.js
+++ javascrip fundamentals, part 1/basic operators/precedence.js
@@ -17,27 +17,55 @@ m tall.
 */
 
 // solution
+/* Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.*/
+// task 1
 // 1. Store Mark's and John's mass and height in variables
-//Mark's mass and height
 const markMass = 78;
 const markHeight = 1.69;
 
-// john's mass and height
+// John BMI
 const johnMass = 92;
-const johnHeight = 1.92;
+const johnHeight = 1.95;
 
+// task 2
 // 2. Calculate both their BMIs using the formula (you can even implement both versions)
 // formula:  BMI = mass / height ** 2 = mass / (height * height)
+const markBMI = markMass / markHeight ** 2;
+console.log(markBMI);
 
-const markBmi = markMass / markHeight ** 2;
-console.log(markBmi);
+// john BMI
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(johnBMI);
 
-const johnBmi = johnMass / johnHeight ** 2;
-console.log(johnBmi);
-
-
+// Task 3
 // 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
-const markHigherBMI = markBmi > johnBmi;
-console.log(markHigherBMI);
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markHeigherBMI);
 
 
+/*
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall 
+*/
+// solution
+// task 1
+// 1. Store Mark's and John's mass and height in variables
+const mark_Mass = 95;
+const mark_Height = 1.88;
+
+const john_Mass = 85;
+const john_Height = 1.79;
+
+// task 2
+// 2. Calculate both their BMIs using the formula (you can even implement both versions)
+// formula:  BMI = mass / height ** 2 = mass / (height * height)
+const mark_BMI = mark_Mass / mark_Height ** 2;
+console.log(mark_BMI);
+
+const john_BMI = john_Mass / john_Height ** 2;
+console.log(john_BMI);
+
+// Task 3
+// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+const mark_Heigher_BMI = mark_BMI > john_BMI;
+console.log(mark_Heigher_BMI);
