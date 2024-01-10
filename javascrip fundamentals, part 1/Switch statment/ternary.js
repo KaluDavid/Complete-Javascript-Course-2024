3c7375c285a7ae33160dd63e9dbf8dd3171dd6f9
Kalu David <kaludavidinyang@gmail.com>
2024-01-10 10:32:51 +0100

    quiz 4

--- javascrip fundamentals, part 1/Switch statment/ternary.js
+++ javascrip fundamentals, part 1/Switch statment/ternary.js
@@ -1,5 +1,3 @@
-
-
 /* Coding Challenge #4
 Steven wants to build a very simple tip calculator for whenever he goes eating in a
 restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
