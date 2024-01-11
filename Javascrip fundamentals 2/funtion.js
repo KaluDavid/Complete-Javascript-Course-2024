6c2c38f03ecd58700c6941bd9b5e3496920d0638
Kalu David <kaludavidinyang@gmail.com>
2024-01-11 22:59:03 +0100

    js function

---
+++ Javascrip fundamentals 2/funtion.js
@@ -0,0 +1,13 @@
+'use strict ';
+
+
+
+// starting with function
+function examScore(broadcasting, editorial) {
+    const bothScore = `In FOB, is scored ${broadcasting}, while in Editorial Writing, I scored ${editorial}`
+
+    return bothScore;
+}
+
+const resultOutput = examScore(68, 81);
+console.log(resultOutput);
