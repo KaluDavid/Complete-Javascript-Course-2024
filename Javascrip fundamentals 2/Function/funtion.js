ba763567c6a27d4f3d21cc23fb91276932d7f028
Kalu David <kaludavidinyang@gmail.com>
2024-01-12 12:40:45 +0100

    function declaration vs expression commit

---
+++ Javascrip fundamentals 2/Function/funtion.js
@@ -0,0 +1,77 @@
+'use strict ';
+
+
+
+// starting with function
+/*
+function examScore(broadcasting, editorial) {
+    const bothScore = `In FOB, I scored ${broadcasting}, while in Editorial Writing, I scored ${editorial}`
+
+    return bothScore;
+}
+
+const resultOutput = examScore(68, 81);
+console.log(resultOutput);
+
+
+
+LECTURE: Functions
+1. Write a function called 'describeCountry' which takes three parameters:
+'country', 'population' and 'capitalCity'. Based on this input, the
+function returns a string with this format: 'Finland has 6 million people and its
+capital city is Helsinki'
+2. Call this function 3 times, with input data for 3 different countries. Store the
+returned values in 3 different variables, and log them to the console
+*
+
+function describeCountry(country, population, capitalCity) {
+    const data = `${country} has ${population} million people and it's capital city is ${capitalCity} `;
+
+    return data;
+}
+
+const finlandData = describeCountry('Finland', 6, 'Helsinki');
+const nigeriaData = describeCountry('Nigeria', +200, 'Abuja');
+const ghanaData = describeCountry('Ghana', 60, 'Accra');
+console.log(finlandData);
+console.log(nigeriaData);
+console.log(ghanaData);
+*/
+
+// FuNction declaration vs Expression
+
+// function declartion
+
+function ageCal(birthYear) {
+    // you can also direction return the function, rather than storing in a var
+    return 2034 - birthYear;
+}
+const realAge = ageCal(2006);
+console.log(realAge);
+
+// Function Expression
+const myAge = function (birthYear) {
+    return 2024 - birthYear;
+}
+const age2 = myAge(2006);
+console.log(age2);
+
+
+
+
+/*
+LECTURE: Function Declarations vs. Expressions
+1. The world population is 7900 million people. Create a function declaration
+called 'percentageOfWorld1' which receives a 'population' value, and
+returns the percentage of the world population that the given population
+represents. For example, China has 1441 million people, so it's about 18.2% of
+the world population
+2. To calculate the percentage, divide the given 'population' value by 7900
+and then multiply by 100
+3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+store the results into variables, and log them to the console
+4. Create a function expression which does the exact same thing, called
+'percentageOfWorld2', and also call it with 3 country populations (can be
+the same populations)*/
+
+
