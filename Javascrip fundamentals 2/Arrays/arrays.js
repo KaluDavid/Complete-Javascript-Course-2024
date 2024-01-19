5bf23d15a5044aa35c22de1f8c5c8a5e645397d9
Kalu David <kaludavidinyang@gmail.com>
2024-01-19 22:41:00 +0100

    commits on arrays

--- Javascrip fundamentals 2/Arrays/arrays.js
+++ Javascrip fundamentals 2/Arrays/arrays.js
@@ -3,3 +3,21 @@
 // building with Arrays
 const friends = ['Dhave', 'Michael', 'Delly', 'Prins'];
 console.log(friends); //this is an array 🙂 
+
+// Another way of creating an Array=  new Array();
+const dept = new Array('linguistics', 'fine art', 'masscom.');
+console.log(dept);
+
+
+// Getting Arrays out
+console.log(friends[0]);//Arrays is zero based
+console.log(friends[3]);
+
+// To know the number of values in an array, you use the .length
+console.log(friends.length);
+
+
+// To the last value in the array
+console.log(friends[friends.length - 1]);
+
+
