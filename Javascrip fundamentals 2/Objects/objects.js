8d784eb6d1a7ed2133cf5016817b7296c1655ea8
Kalu David <kaludavidinyang@gmail.com>
2024-01-23 07:49:20 +0100

    objects

--- Javascrip fundamentals 2/Objects/objects.js
+++ Javascrip fundamentals 2/Objects/objects.js
@@ -1,1 +1,14 @@
-!
+'use strict';
+
+
+// Creating an object
+// e.g
+// give the details of Danny
+const danny = {
+    fName: 'Danny',
+    lName: 'Fred',
+    age: 2000 - 2032,
+    friends: ['Dhave', 'Winny', 'Michael']
+}//fundamental way of creating an object
+
+console.log(danny);
