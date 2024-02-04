23628542cdf597a0aa39f481dd3e132d478f2a1e
Kalu David <kaludavidinyang@gmail.com>
2024-02-04 21:09:26 +0100

    Loops

--- Javascrip fundamentals 2/Objects/objects.js
+++ Javascrip fundamentals 2/Objects/objects.js
@@ -48,18 +48,51 @@ console.log(danny['firstName']);
 
 
 // Objects Method
-const dhaveData = {
+const david = {
     firstName: 'dhave',
     birthYear: 1970,
     friends: ['michael', 'delly', 'gilbert', 'danny'],
     job: 'software engineer',
 
     // let's calculate the age
-    calAge : function() {
+    calage: function () {
         console.log(this)// the this keyword, points to the object identifier
         return 2024 - this.birthYear;
     }
 }
 
-console.log(dhaveData.calAge());
+console.log(david.calage());
+
+// David is a 46 year olf teacher and he has a drivers license
+
+/*
+CHALLENGE #3
+Let's go back to Mark and John comparing their BMIs!
+
+This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your tasks:
+
+For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property, and also return it from the method.
+
+Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+*/
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+
+};
+
+const John = {
+    fullName: 'John Smith',
+   mass: 92,
+   height: 1.
+
+};
 
