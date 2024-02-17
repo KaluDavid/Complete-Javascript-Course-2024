15dc27232c8d399d8b1003b62aa64f9329496a6c
Kalu David <kaludavidinyang@gmail.com>
2024-02-17 21:45:44 +0100

    loop commit

--- Javascrip fundamentals 2/Loops/forloop.js
+++ Javascrip fundamentals 2/Loops/forloop.js
@@ -10,8 +10,8 @@ for (let rep = 1; rep <= 10; rep++) {
 const davidData = [
   "John",
   "David",
+  2024 - 1999,
   ["Michael", "Elsa", "Danny"],
-  1999 - 2024,
   "Programmer",
   "i Love to code....",
 ];
@@ -31,5 +31,69 @@ for (let i = 0; i < davidData.length; i++) {
 }
 console.log(types);
 
+console.log(`
 
+
+`);
 // Now Lets try out something
+// lets say we wanna calculate 3 birth years
+const birthYear = [1960, 1970, 1999, 2004];
+
+const ages = []; //the empty string where the birth year calculation will be stored
+// lets loop
+for (let years = 0; years < birthYear.length; years++) {
+  ages.push(2023 - birthYear[years]); //now this calculation will print out each of the years one by one
+}
+console.log(ages);
+
+// Up Next lets try out the Continue and Break statement
+
+//CONTINUE STATEMENT
+console.log("--- CONTINUE STATEMENT ---");
+for (let i = 0; i < davidData.length; i++) {
+  // lets add conditionals
+  if (typeof davidData[i] !== "string") continue;
+
+  console.log(davidData[i], typeof davidData[i]);
+}
+
+//BREAK STATEMENT
+console.log("--- BREAK STATEMENT ---");
+//
+for (let i = 0; i < davidData.length; i++) {
+  // lets add conditionals
+  if (typeof davidData[i] === "number") break;
+  console.log(davidData[i], typeof davidData[i]);
+}
+
+// LOOPING BACKWARD AND LOOP IN LOOP
+
+console.log(`
+
+LOOPING BACKWARD AND LOOP IN LOOP
+
+`);
+// so here, instead of the normal way it is read 0,1,2....., it is read backwards 2,1,0
+// e.g
+
+for (let i = davidData.length - 1; i >= 0; i--) {
+  // Reading from david arrays
+  console.log(i, davidData[i], typeof davidData[i]);
+}
+
+console.log(`
+
+// LOOPS IN LOOPS
+
+`);
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`__________ Starting exercise ${exercise}__________`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`I am good at football ${rep}`);
+
+    for (let ball = 1; ball < 3; ball++) {
+      console.log(`I'm a baller ${ball}`);
+    }
+  }
+}
