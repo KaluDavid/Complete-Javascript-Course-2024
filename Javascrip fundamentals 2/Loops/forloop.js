89d58e11b33ad7eac282ab1222a4c33d84589a34
Kalu David <kaludavidinyang@gmail.com>
2024-02-22 19:24:19 +0100

    loops

--- Javascrip fundamentals 2/Loops/forloop.js
+++ Javascrip fundamentals 2/Loops/forloop.js
@@ -1,127 +1,135 @@
 "use strict";
-// the for loop
-for (let rep = 1; rep <= 10; rep++) {
-  console.log("I love to code");
-}
+// // the for loop
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log("I love to code");
+// }
 
-// LOOPING ARRAYS, BREAKING AND CONTINUING
+// // LOOPING ARRAYS, BREAKING AND CONTINUING
 
-// so lets create an array
-const davidData = [
-  "John",
-  "David",
-  2024 - 1999,
-  ["Michael", "Elsa", "Danny"],
-  "Programmer",
-  "i Love to code....",
-];
+// // so lets create an array
+// const davidData = [
+//   "John",
+//   "David",
+//   2024 - 1999,
+//   ["Michael", "Elsa", "Danny"],
+//   "Programmer",
+//   "i Love to code....",
+// ];
 
-const types = []; // here the new array types will be based on the davidData array
-// its gonna have the same length
+// const types = []; // here the new array types will be based on the davidData array
+// // its gonna have the same length
 
-for (let i = 0; i < davidData.length; i++) {
-  // Reading from david arrays
-  console.log(davidData[i], typeof davidData[i]);
+// for (let i = 0; i < davidData.length; i++) {
+//   // Reading from david arrays
+//   console.log(davidData[i], typeof davidData[i]);
 
-  // filling the types arrays
-  // types[i] = typeof davidData[i];
+//   // filling the types arrays
+//   // types[i] = typeof davidData[i];
 
-  // lets try the .push method
-  types.push(davidData[i]);
-}
-console.log(types);
+//   // lets try the .push method
+//   types.push(davidData[i]);
+// }
+// console.log(types);
 
-console.log(`
+// console.log(`
 
+// `);
+// // Now Lets try out something
+// // lets say we wanna calculate 3 birth years
+// const birthYear = [1960, 1970, 1999, 2004];
 
-`);
-// Now Lets try out something
-// lets say we wanna calculate 3 birth years
-const birthYear = [1960, 1970, 1999, 2004];
+// const ages = []; //the empty string where the birth year calculation will be stored
+// // lets loop
+// for (let years = 0; years < birthYear.length; years++) {
+//   ages.push(2023 - birthYear[years]); //now this calculation will print out each of the years one by one
+// }
+// console.log(ages);
 
-const ages = []; //the empty string where the birth year calculation will be stored
-// lets loop
-for (let years = 0; years < birthYear.length; years++) {
-  ages.push(2023 - birthYear[years]); //now this calculation will print out each of the years one by one
-}
-console.log(ages);
+// // Up Next lets try out the Continue and Break statement
 
-// Up Next lets try out the Continue and Break statement
+// //CONTINUE STATEMENT
+// console.log("--- CONTINUE STATEMENT ---");
+// for (let i = 0; i < davidData.length; i++) {
+//   // lets add conditionals
+//   if (typeof davidData[i] !== "string") continue;
 
-//CONTINUE STATEMENT
-console.log("--- CONTINUE STATEMENT ---");
-for (let i = 0; i < davidData.length; i++) {
-  // lets add conditionals
-  if (typeof davidData[i] !== "string") continue;
+//   console.log(davidData[i], typeof davidData[i]);
+// }
 
-  console.log(davidData[i], typeof davidData[i]);
-}
+// //BREAK STATEMENT
+// console.log("--- BREAK STATEMENT ---");
+// //
+// for (let i = 0; i < davidData.length; i++) {
+//   // lets add conditionals
+//   if (typeof davidData[i] === "number") break;
+//   console.log(davidData[i], typeof davidData[i]);
+// }
 
-//BREAK STATEMENT
-console.log("--- BREAK STATEMENT ---");
-//
-for (let i = 0; i < davidData.length; i++) {
-  // lets add conditionals
-  if (typeof davidData[i] === "number") break;
-  console.log(davidData[i], typeof davidData[i]);
-}
+// // LOOPING BACKWARD AND LOOP IN LOOP
+
+// console.log(`
 
 // LOOPING BACKWARD AND LOOP IN LOOP
 
-console.log(`
+// `);
+// // so here, instead of the normal way it is read 0,1,2....., it is read backwards 2,1,0
+// // e.g
 
-LOOPING BACKWARD AND LOOP IN LOOP
+// for (let i = davidData.length - 1; i >= 0; i--) {
+//   // Reading from david arrays
+//   console.log(i, davidData[i], typeof davidData[i]);
+// }
 
-`);
-// so here, instead of the normal way it is read 0,1,2....., it is read backwards 2,1,0
-// e.g
+// console.log(`
 
-for (let i = davidData.length - 1; i >= 0; i--) {
-  // Reading from david arrays
-  console.log(i, davidData[i], typeof davidData[i]);
-}
+// // LOOPS IN LOOPS
 
-console.log(`
+// `);
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`__________ Starting exercise ${exercise}__________`);
 
-// LOOPS IN LOOPS
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: I am good at football ${rep}`);
+//   }
+// }
 
-`);
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`__________ Starting exercise ${exercise}__________`);
+// console.log(`
 
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise ${exercise}: I am good at football ${rep}`);
-  }
-}
+// ___________WHILE LOOP____________
 
-console.log(`
+// `);
+// // WHILE LOOP
+// let rep = 1; /*the initial counter repetition value */
+// while (rep <= 5 /* the condition*/) {
+//   console.log(`I love to play ${rep} `);
 
+//   rep++; //the counter increment
+// }
 
+// // lets take up another example
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
 
-___________WHILE LOOP____________
+//   if (dice === 6) {
+//     console.log("Loop is ending....");
+//   }
+// }
 
-`);
-// WHILE LOOP
-let rep = 1; /*the initial counter repetition value */
-while (rep <= 5 /* the condition*/) {
-  console.log(`I love to play ${rep} `);
+// Problem Solving
 
-  rep++; //the counter increment
-}
+//We Work for a company building a smart home thermometer. Our most risked task is this: Given an Array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error
 
-// lets take up another example
-let dice = Math.trunc(Math.random() * 6 )+ 1;
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-  if (dice === 6) {
-    console.log("Loop is ending....");
-  }
-}
+// solution
+//1) Understand the problem
+// -- What is the temperature amplitude? Answer: Difference between highest and lowest temp
+//-- How to compute max and min temperatures
+//-- WHat's a sensor error and what to do when the problem occur
 
-
-
-
-
-
-
+// 2) Break the problems into smaller bit
+// -- How to ignore errors?
+// -- Find max value in temperature array
+// -- Find Min value
+// -- Subtract min from Max (amplitude) then return it
