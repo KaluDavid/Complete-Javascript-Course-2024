0c28e00adf5208175aeb2efe96453503ed3dfed3
Kalu David <kaludavidinyang@gmail.com>
2024-02-23 16:39:43 +0100

    dev skills

--- Javascrip fundamentals 2/Loops/forloop.js
+++ Javascrip fundamentals 2/Loops/forloop.js
@@ -133,3 +133,46 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // -- Find max value in temperature array
 // -- Find Min value
 // -- Subtract min from Max (amplitude) then return it
+//
+
+// Lets start building
+
+const calTemAmplitudeNew = function (t1, t2) {
+  const temp = t1.concat(t2);
+  console.log(temp);
+
+  let max = temp[0];
+  let min = temp[0];
+
+  for (let i = 0; i < temp.length; i++) {
+    const curTemp = temp[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calTemAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+// This is how it works
+// In the beginning, before we start the array, the max (let max) will be the first element of the array (3)
+// max = 3
+// Then the loops starts the first position
+// the we ask // is temp[i] > max
+// is 3 greater than 3 (thats a no, nothing happens)
+// then in the next iteration: is 7 greater than 3 (yes)
+// Max = 7
+// then in the next iteration: is 4 greater than 7 (no, nothing happens)
+// Therefore, we're done
+// Then the result in the end is  Max = 7
+
+// Problem 2
+// Function should now recieve 2 arrays of temps
+
+// 1) Understanding the problem
+// -- With 2 arrays, should w implement functionality twice? NO! just merge two arrays
+
+// 2) Breaking the problems
+// -- How to merge two arrays?p0
