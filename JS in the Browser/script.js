2635cad1cab5e2edca5dcb80858cfdfb6389e9ab
Kalu David <kaludavidinyang@gmail.com>
2024-02-27 22:00:49 +0100

    dom manipulation 🤗

---
+++ JS in the Browser/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// lets manipulate some html content
+console.log(document.querySelector(".message").textContent);
+// lets change the content
+document.querySelector('.message').textContent='✨😃Correct Winner';
+
+document.querySelector('.number').textContent= 13;
+document.querySelector('.score').textContent= 10;
+
+
