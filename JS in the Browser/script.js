3e59a4b8530eebed2d19d1a397b8b8e9ce17904a
Kalu David <kaludavidinyang@gmail.com>
2024-02-28 22:18:55 +0100

    change

--- JS in the Browser/script.js
+++ JS in the Browser/script.js
@@ -1,11 +1,21 @@
 "use strict";
 
+/*
 // lets manipulate some html content
 console.log(document.querySelector(".message").textContent);
 // lets change the content
-document.querySelector('.message').textContent='✨😃Correct Winner';
+document.querySelector(".message").textContent = "✨😃Correct Winner";
 
-document.querySelector('.number').textContent= 13;
-document.querySelector('.score').textContent= 10;
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
 
+// manipulating the input tag
+document.querySelector(".guess").value = 10;
+console.log((document.querySelector(".guess").value = 10));
 
+*/
+
+// lets now work on the project
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
