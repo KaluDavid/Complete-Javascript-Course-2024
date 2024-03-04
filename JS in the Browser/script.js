22d7a12c6f055633e1b686091c17183f47c79111
Kalu David <kaludavidinyang@gmail.com>
2024-03-04 22:55:08 +0100

    lots of style

--- JS in the Browser/script.js
+++ JS in the Browser/script.js
@@ -19,12 +19,10 @@ console.log((document.querySelector(".guess").value = 10));
 
 // so here, basically we need to a secret number btw 0-20
 const computerGuess = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = computerGuess;
 
 let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
-
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
@@ -35,7 +33,10 @@ document.querySelector(".check").addEventListener("click", function () {
     // When Number is correct and wins
   } else if (guess === computerGuess) {
     document.querySelector(".message").textContent = "✨Correct Number";
-
+    document.querySelector(".number").textContent = computerGuess;
+    // lets add some styling if user wins
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
 
     // when guess is too high
   } else if (guess > computerGuess) {
@@ -46,6 +47,9 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       document.querySelector(".message").textContent = "⛔You lose";
       document.querySelector(".score").textContent = 0;
+      // lets add some styling if user loses
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".number").style.width = "15rem";
     }
 
     // when guess is too Low
@@ -57,7 +61,9 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       document.querySelector(".message").textContent = "⛔You lose";
       document.querySelector(".score").textContent = 0;
+      // lets add some styling if user lose
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".number").style.width = "15rem";
     }
   }
-
 });
