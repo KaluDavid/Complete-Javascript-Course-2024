313f46ee861f9aba188d13c800c2aad8f56570da
Kalu David <kaludavidinyang@gmail.com>
2024-03-24 00:47:23 +0100

    implementing dice functionality

--- JS in the Browser/Pig-Game/script.js
+++ JS in the Browser/Pig-Game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting Element
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// STARTING CONDITION
+score0El.textContent = Number(0);
+score1El.textContent = Number(0);
+// diceEl.classList = '.hidden';
