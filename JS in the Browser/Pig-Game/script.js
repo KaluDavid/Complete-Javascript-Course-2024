4ca974de7556a745933a2dd05227f2ef92f5079c
Kalu David <kaludavidinyang@gmail.com>
2024-03-26 22:00:16 +0100

    added changes

--- JS in the Browser/Pig-Game/script.js
+++ JS in the Browser/Pig-Game/script.js
@@ -4,6 +4,13 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 // STARTING CONDITION
 score0El.textContent = Number(0);
@@ -14,6 +21,7 @@ diceEl.style.display = 'none';
 let currentScore = 0;
 let activePlayer = 0;
 const score = [0, 0];
+
 // Rolling The dice functionality
 btnRoll.addEventListener('click', function () {
   //1- Generate a random dice roll
